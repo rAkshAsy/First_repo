5610eea87435f1724547a29a0c459571d8624948
Vladislav Hnyloskurenko <v.hniloskurenko@gmail.com>
2025-06-15 21:32:56 +0300

    Create mamble function (CodeWars)

--- cwpract.py
+++ cwpract.py
@@ -1,30 +1,25 @@
-'''
-In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+"""
+This time no story, no theory. The examples below show you how to write function accum:
 
-Examples
-high_and_low("1 2 3 4 5") # return "5 1"
-high_and_low("1 2 -3 4 5") # return "5 -3"
-high_and_low("1 9 3 4 -5") # return "9 -5"
-Notes
-All numbers are valid Int32, no need to validate them.
-There will always be at least one number in the input string.
-Output string must be two numbers separated by a single space, and highest number is first.
-'''
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+"""
 
-def high_and_low(numbers:str):
-    numbers = numbers.split()
-    higher, lower = int(numbers[0]), int(numbers[0])
-    for num in numbers:
-        higher = int(num) if int(num) > higher else higher
-        lower = int(num) if int(num) < lower else lower
-        
-    return f'{higher} {lower}'
+def accum(st:str) ->str:
+    res_str = ''
+    counter = 1
 
-# Better issues  
-# def high_and_low(numbers):
-#   numbers = [int(c) for c in numbers.split(' ')]
-#   return f"{max(numbers)} {min(numbers)}"
+    for char in st:
+        char = char.casefold()*counter
+        res_str += f'{char.capitalize()}' if counter == 1 else f'-{char.capitalize()}'
+        counter += 1
+    return res_str
 
+# Better version // enumerate - can help to give indexes in i and character in a 
+# def accum(s):
+#     return '-'.join((a * i).title() for i, a in enumerate(s, 1))
 
-print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4")) # 42 -9
-print(high_and_low("1 2 3")) # 3 1
+print(accum('ZpglnRxqenU'))
